8988489da2aa0595cd193d4fe32ce93dfd458481
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-09-24 15:14:44 +0300

    Make the calendar days buttons

--- src/components/Calendar/Day.tsx
+++ src/components/Calendar/Day.tsx
@@ -4,15 +4,20 @@ import classNames from 'classnames';
 import classes from './index.module.css';
 
 interface CalendarDayProps
-  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   day: number;
 }
 
 const Day = ({ day, className, ...props }: CalendarDayProps) => {
-  return (
-    <span className={classNames(classes.day, className)} {...props}>
-      {day < 1 ? '' : day}
-    </span>
+  return day < 1 ? (
+    <span className={classes.day} />
+  ) : (
+    <button className={classNames(classes.day, className)} {...props}>
+      {day}
+    </button>
   );
 };
 
