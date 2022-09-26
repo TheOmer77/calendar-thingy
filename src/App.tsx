e8a99ebf80bdcfe35903d201c9a22f8eab413a9a
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-09-26 15:08:46 +0300

    Improve App styling, add text showing current `selectedDate` state

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { Calendar } from './components/Calendar';
 
 import './App.css';
 
 const today = new Date();
+const locale = 'en-US';
 
 const App = () => {
   const [selectedDate, setSelectedDate] = useState<Date>();
 
+  const formatDate = useMemo(
+    () =>
+      new Intl.DateTimeFormat(locale, {
+        dateStyle: 'full',
+      }).format,
+    []
+  );
+
   return (
-    <Calendar
-      value={selectedDate}
-      onChange={newDate => setSelectedDate(newDate)}
-      minDate={new Date(today.getTime() - 8640000000)}
-      maxDate={new Date(today.getTime() + 8640000000)}
-    />
+    <>
+      <div className='calendar-display'>
+        <h1 className='calendar-display-title'>Calendar</h1>
+        <div className='selected-date-text'>
+          {selectedDate
+            ? `Selected date:\n${formatDate(selectedDate)}`
+            : 'Click on a date to select it.'}
+        </div>
+        <Calendar
+          value={selectedDate}
+          onChange={newDate => setSelectedDate(newDate)}
+          minDate={new Date(today.getTime() - 8640000000)}
+          maxDate={new Date(today.getTime() + 8640000000)}
+          locale={locale}
+        />
+      </div>
+    </>
   );
 };
 
