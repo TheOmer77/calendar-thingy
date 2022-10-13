5a6fb7d87384c46322a5a381047b0dca25e4c5e2
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-10-14 00:14:41 +0300

    Tweaks for package build 🤔

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'CalendarThingy',
+      fileName: 'index',
     },
     rollupOptions: {
       external: ['classnames', 'react', 'react-dom', 'react-window'],
