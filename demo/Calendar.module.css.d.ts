e0c646038a1fb8a08e6f7dd4884eb02520658d4a
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-10-13 17:06:05 +0300

    Add my custom styling to demo

---
+++ demo/Calendar.module.css.d.ts
@@ -0,0 +1,22 @@
+declare const styles: {
+  readonly "calendar": string;
+  readonly "day-selected": string;
+  readonly "day-button": string;
+  readonly "year-picker": string;
+  readonly "year-picker-row": string;
+  readonly "year-button": string;
+  readonly "year-selected": string;
+  readonly "day-in-range": string;
+  readonly "day-range": string;
+  readonly "day-start": string;
+  readonly "day-end": string;
+  readonly "calendar-header": string;
+  readonly "arrow-switcher": string;
+  readonly "days-header": string;
+  readonly "week": string;
+  readonly "month": string;
+  readonly "day-label": string;
+  readonly "day": string;
+};
+export = styles;
+
